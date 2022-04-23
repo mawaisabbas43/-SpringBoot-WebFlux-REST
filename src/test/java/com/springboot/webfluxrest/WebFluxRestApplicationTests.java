2dd79dc2fb4d0ae2ab266ae4b61443f16242be21
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-04-23 11:19:31 +0500

    Project added

---
+++ src/test/java/com/springboot/webfluxrest/WebFluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
