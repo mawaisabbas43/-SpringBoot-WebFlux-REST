e9e79d3b52fa57a8099cb30a2286de1d07df237a
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-04-24 05:55:33 +0500

    Controller and test added for Vendor

---
+++ src/test/java/com/springboot/webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,52 @@
+package com.springboot.webfluxrest.controllers;
+
+import com.springboot.webfluxrest.domain.Vendor;
+import com.springboot.webfluxrest.repositories.VendorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController controller;
+
+    @BeforeEach
+    public void setUp() {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        controller = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(controller).build();
+    }
+
+    @Test
+    public void list() {
+
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Fred").lastName("Flintstone").build(),
+                        Vendor.builder().firstName("Barney").lastName("Rubble").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(vendorRepository.findById("someid"))
+                .willReturn(Mono.just(Vendor.builder().firstName("Jimmy").lastName("Johns").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/someid")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
